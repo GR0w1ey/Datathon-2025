0a3fd08333b9d3ae51337c70a2f8687df1ccb1b8
constancevielma <c.vielma.clegg@accenture.com>
2025-06-25 14:18:51 +0100

    made new csv for feature analysis, waiting to merge

--- sql_scripts/joining_tables.sql
+++ sql_scripts/joining_tables.sql
@@ -2,5 +2,5 @@ SELECT *
 FROM PLAYGROUND.DATATHON.RACES AS race
 FULL JOIN PLAYGROUND.DATATHON.RESULTS res ON res.RACEID = race.RACEID -- full join as we want ALL the races, even with no results? do we?
 JOIN PLAYGROUND.DATATHON.DRIVERS driver ON driver.driverid = res.driverid
-WHERE race.year = 2024
+WHERE race.year < 2025 AND race.year > 1998
 ORDER BY race.raceid, res.position;
--- sql_scripts/new_script.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM PLAYGROUND.DATATHON.DRIVERS;
-
--- Results and races
--- No country code, no url, no number, 
